95d96d69d02768f5f242d8819337e3c7f9dd673b
Tymoteusz Barczynski <tymoteusz.barczynski@gmail.com>
2026-07-09 14:53:54 +0200

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.py
@@ -0,0 +1,25 @@
+from math import ceil
+from typing import List
+
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def hours_needed(eating_speed: int) -> int:
+            total_time = 0
+
+            for pile in piles:
+                total_time += ceil(pile / eating_speed)
+
+            return total_time
+
+        left_time, right_time = 1, max(piles)
+
+        while left_time <= right_time:
+            mid = (left_time + right_time) // 2
+
+            if hours_needed(mid) <= h:
+                right_time = mid - 1
+            else:
+                left_time = mid + 1
+
+        return left_time
