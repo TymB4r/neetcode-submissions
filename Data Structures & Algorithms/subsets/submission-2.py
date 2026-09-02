ecd903614f1593daff6ebc0755a5271eb4acda12
Tymoteusz Barczynski <tymoteusz.barczynski@gmail.com>
2026-09-02 13:59:09 +0200

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        result = []
+
+        def backtrack(cur_set: List[int], idx: int) -> None:
+            if idx < n:
+                backtrack(cur_set, idx+1)
+                backtrack(cur_set+[nums[idx]], idx+1)
+            else:
+                result.append(cur_set)
+
+        backtrack([], 0)
+        return result
