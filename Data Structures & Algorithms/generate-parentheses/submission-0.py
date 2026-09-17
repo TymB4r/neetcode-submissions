25e880ed497dd11add6013dc00c39c556e878dbe
Tymoteusz Barczynski <tymoteusz.barczynski@gmail.com>
2026-09-18 00:07:22 +0200

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        def backtrack(cur_par: List[int], op_brackets: int, cl_brackets: int) -> None:
+            if op_brackets == n and cl_brackets == n:
+                res.append(''.join(cur_par))
+                return
+            
+            if op_brackets < n:
+                backtrack(cur_par + ['('], op_brackets + 1, cl_brackets)
+            
+            if cl_brackets < op_brackets:
+                backtrack(cur_par + [')'], op_brackets, cl_brackets + 1)
+        
+        backtrack([], 0, 0)
+        return res
